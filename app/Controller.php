dc7a3c3537aff74404c873e0a654594f8a905224
Alexandre-JAMROZ <alexandrejamrozpro@gmail.com>
2026-05-11 20:24:17 +1100

    ajout: contrôleur de base et articles

---
+++ app/Controller.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace app;
+
+#[\AllowDynamicProperties]
+abstract class Controller {
+    
+}
---
+++ controllers/Articles.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace controllers;
+class Articles extends \app\Controller {
+    /**
+     * Cette méthode affiche la liste des articles
+     * 
+     * @return void
+     */
+    public function index() {
+        echo "Ici nous aurons la liste des articles";
+    }
+}
