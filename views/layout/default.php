cc037a25e676f25ebc499a8ef9ce624ee28e94d7
Alexandre-JAMROZ <alexandrejamrozpro@gmail.com>
2026-05-12 22:47:12 +1100

    ajout: images, styles, polices | modif: layout default

--- views/layout/default.php
+++ views/layout/default.php
@@ -1,21 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Blog</title>
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="stylesheet" href="/public/css/styles.css">
 </head>
-<body>
-    <header>
-        <h1>Bienvenue sur mon blog</h1>
-    </header>
 
-    <main>
-        <?= $content ?>
-    </main>
-    
-    <footer>
-        <p>Copyright 2026</p>
-    </footer>
+<body>
+    <div id="page">
+        <header>
+            <nav>
+                <a href="/">
+                    <img class="nav-logo" src="/public/img/logo.png" width="100">
+                </a>
+                <ul>
+                    <li><a href="/">Accueil</a></li>
+                    <li><a href="">Jeux vidéos</a></li>
+                    <li><a href="">Sports</a></li>
+                    <li><a href="">Informatique</a></li>
+                </ul>
+                <a href="#contact">Contact</a>
+            </nav>
+        </header>
+
+        <main>
+            <?= $content ?>
+        </main>
+
+        <footer>
+            <p>&copy; Copyright <?= date("Y") ?> — Alexandre JAMROZ</p>
+        </footer>
+    </div>
 </body>
+
 </html>
