0fade58e33dd9d48a3eeeab12932278496664658
Alexandre-JAMROZ <alexandrejamrozpro@gmail.com>
2026-05-12 13:36:18 +1100

    ajout: appel du model dans les contrôleurs et modèle article

--- controllers/Articles.php
+++ controllers/Articles.php
@@ -8,6 +8,13 @@ class Articles extends \app\Controller {
      * @return void
      */
     public function index() {
-        echo "Ici nous aurons la liste des articles";
+        // On instancie le modèle "Articles"
+        $this->loadModel("Articles");
+
+        // On stocke la liste des articles dans $articles
+        $articles = $this->Articles->getAll();
+
+        // On affiche les données
+        $this->render("index", compact("articles"));
     }
 }
